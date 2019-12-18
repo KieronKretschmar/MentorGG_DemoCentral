a5064095f074b67d9d81a8b4799794ad6ded518a
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2019-12-18 15:39:15 +0100

    Add gatherrer sending message to demo downloader

--- RabbitCommunication/Gatherer.cs
+++ RabbitCommunication/Gatherer.cs
@@ -7,10 +7,13 @@ namespace DemoCentral.RabbitCommunication
     public class Gatherer : Consumer<GathererTransferModel>
     {
         private readonly IDemoCentralDBInterface _dbInterface;
+        private readonly DemoDownloader _demoDownloader;
 
-        public Gatherer(IQueueConnection queueConnection, IDemoCentralDBInterface dbInterface) : base(queueConnection)
+        public Gatherer(IQueueConnection queueConnection, IDemoCentralDBInterface dbInterface, DemoDownloader demoDownloader) : base(queueConnection)
         {
+
             _dbInterface = dbInterface;
+            _demoDownloader = demoDownloader;
         }
 
         protected override void HandleMessage(IBasicProperties properties, GathererTransferModel model)
@@ -20,7 +23,10 @@ namespace DemoCentral.RabbitCommunication
             //TODO handle duplicate entry, currently not inserted into db and forgotten afterwards
             if (_dbInterface.TryCreateNewDemoEntryFromGatherer(matchId, model))
             {
-                //TODO Send to DemoDownloader
+                var forwardModel = new DC_DD_Model { 
+                    DownloadUrl = model.DownloadUrl
+                };
+                _demoDownloader.PublishMessage(properties.CorrelationId, forwardModel);
             }
         }
     }
