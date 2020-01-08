79927dd6e92db29985e5807dfa9d78a1e0ba2425
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-01-08 11:41:21 +0100

    Add comments and BadRequestResult; Update endpoint comment fixes #14 #15

--- DemoCentral/Controllers/exposed/QueueController.cs
+++ DemoCentral/Controllers/exposed/QueueController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Web;
+using System.Net;
 
 namespace DemoCentral.Controllers.exposed
 {
@@ -18,18 +20,42 @@ namespace DemoCentral.Controllers.exposed
             _dbInterface = dbInterface;
         }
 
+        /// <summary>
+        /// Get the position in queue for a certain demo 
+        /// </summary>
+        /// <param name="matchId">id of the certain demo</param>
+        /// <returns>either int or BadRequest if the demo could not be found</returns>
         [HttpGet("{matchId}")]
         // GET /api/exposed/queue/playermatches/1
-        public int QueuePosition(long matchId)
+        public ActionResult<int> QueuePosition(long matchId)
         {
-            return _dbInterface.GetQueuePosition(matchId);
+            try
+            {
+                return new ActionResult<int>(_dbInterface.GetQueuePosition(matchId));
+            }
+            catch (InvalidOperationException)
+            {
+                return new BadRequestResult();
+            }
         }
 
+        /// <summary>
+        /// Get the number of enqueued matches for a certain player 
+        /// </summary>
+        /// <param name="playerId">steamid of the certain player</param>
+        /// <returns>either int or BadRequest if the demo could not be found</returns>
         [HttpGet("[action]/{playerId}")]
-        // GET /api/exposed/queue/playermatches/1
-        public int NumberPlayerMatches(long playerId)
+        // GET /api/exposed/queue/numberplayermatches/1
+        public ActionResult<int> NumberPlayerMatches(long playerId)
         {
-            return _dbInterface.GetPlayerMatchesInQueue(playerId).Count;
+            try
+            {
+                return new ActionResult<int>(_dbInterface.GetPlayerMatchesInQueue(playerId).Count);
+            }
+            catch (InvalidOperationException)
+            {
+                return new BadRequestResult();
+            }
         }
     }
 
