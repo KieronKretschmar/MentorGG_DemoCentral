3631d3aa609541af9c57aaf18d3db817c889ed58
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-12 16:57:14 +0100

    Restructure communications folder to allow for http service

---
+++ DemoCentral/Communication/HTTP/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCentral.Communication.HTTP
+{
+    public class UserInfo
+    {
+    }
+}
