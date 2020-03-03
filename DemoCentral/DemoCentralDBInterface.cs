0645ee8759a18a904e4e5d678f6bf6c398ef1fd2
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-03-03 16:48:12 +0100

    Update interface

--- DemoCentral/DemoCentralDBInterface.cs
+++ DemoCentral/DemoCentralDBInterface.cs
@@ -16,30 +16,38 @@ namespace DemoCentral
     /// </summary>
     public interface IDemoCentralDBInterface
     {
-        void SetFilePath(long matchId, string zippedFilePath);
         DemoAnalyzeInstructions CreateAnalyzeInstructions(long matchId);
+        Demo GetDemoById(long matchId);
         /// <summary>
         /// Returns the player matches in queue , empty list if none found
         /// </summary>
         List<Demo> GetRecentMatches(long playerId, int recentMatches, int offset = 0);
         List<long> GetRecentMatchIds(long playerId, int recentMatches, int offset = 0);
         bool IsReanalysisRequired(string hash, out long matchId, byte framesPerSecond = 1);
+        void RemoveDemo(Demo demo);
         void RemoveDemo(long matchId);
-        string SetDownloadRetryingAndGetDownloadPath(long matchId);
-        void SetFileStatus(long matchId, FileStatus status);
-        void SetUploadStatus(long matchId, bool success);
+        void SetDatabaseVersion(Demo demo, string databaseVersion);
         void SetDatabaseVersion(long matchId, string databaseVersion);
+        string SetDownloadRetryingAndGetDownloadPath(Demo demo);
+        string SetDownloadRetryingAndGetDownloadPath(long matchId);
+        void SetFilePath(Demo demo, string zippedFilePath);
+        void SetFilePath(long matchId, string zippedFilePath);
+        void SetFileStatus(Demo demo, FileStatus status);
+        void SetFileStatus(long matchId, FileStatus status);
+        void SetFileWorkerStatus(Demo demo, DemoFileWorkerStatus status);
         void SetFileWorkerStatus(long matchId, DemoFileWorkerStatus status);
-
+        void SetFrames(Demo demo, int framesPerSecond);
+        void SetFrames(long matchId, int framesPerSecond);
+        void SetHash(Demo demo, string hash);
+        void SetHash(long matchId, string hash);
+        void SetUploadStatus(Demo demo, bool success);
+        void SetUploadStatus(long matchId, bool success);
         /// <summary>
         /// try to create a new entry in the demo table. Returns false and the matchId of the match, if the downloadUrl is already known, return true otherwise
         /// </summary>
         /// <param name="matchId">Return either a new matchId or the one of the found demo if the download url is known</param>
         /// <returns>true, if downloadUrl is unique</returns>
         bool TryCreateNewDemoEntryFromGatherer(DemoEntryInstructions model, AnalyzerQuality requestedQuality, out long matchId);
-        void SetHash(long matchId, string hash);
-        void SetFrames(long matchId, int framesPerSecond);
-        Demo GetDemoById(long matchId);
     }
 
     /// <summary>
@@ -181,7 +189,7 @@ namespace DemoCentral
             return downloadUrl;
         }
 
-       
+
 
         /// <summary>
         /// Checks if a hash is already in the database, and analyzed with more frames than the requested amount \n
