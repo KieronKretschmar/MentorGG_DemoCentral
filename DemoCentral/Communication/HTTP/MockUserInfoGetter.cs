d33b4106acaa498adad19a401cfe7db1bd23e4c5
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-03-27 18:31:32 +0100

    Add UserInfoGetter and Mock

---
+++ DemoCentral/Communication/HTTP/MockUserInfoGetter.cs
@@ -0,0 +1,24 @@
+﻿using DemoCentral.Communication.HTTP;
+using Microsoft.Extensions.Logging;
+using RabbitCommunicationLib.Enums;
+using System.Threading.Tasks;
+
+namespace DemoCentral.Communication.HTTP
+{
+    internal class MockUserInfoGetter : IUserInfoGetter
+    {
+        private readonly ILogger<MockUserInfoGetter> _logger;
+
+        public MockUserInfoGetter(ILogger<MockUserInfoGetter> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<AnalyzerQuality> GetAnalyzerQualityAsync(long steamId)
+        {
+            _logger.LogWarning($"You are using the mock for UserInfoGatherer. Request made for SteamId [ {steamId} ]");
+            return Task.FromResult(AnalyzerQuality.Medium);
+        }
+    }
+}
+
