aa71f650822a68187290ab8e4bda29e8249b8269
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-04-01 16:46:51 +0200

    Add new controller for convenience methods

---
+++ DemoCentral/Controllers/trusted/ConvenienceController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoCentral.Communication.Rabbit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitCommunicationLib.TransferModels;
+
+namespace DemoCentral.Controllers.trusted
+{
+    [ApiVersion("1")]
+    [Route("v{version:apiVersion}/trusted")]
+    [ApiController]
+    public class ConvenienceController : ControllerBase
+    {
+        private readonly ILogger<ConvenienceController> _logger;
+        private readonly IMatchWriter _matchWriter;
+
+        public ConvenienceController(ILogger<ConvenienceController> logger, IMatchWriter matchWriter)
+        {
+            _logger = logger;
+            _matchWriter = matchWriter;
+        }
+
+        /// <summary>
+        /// Remove a demo from the match database
+        /// </summary>
+        /// <param name="matchId">match to remove</param>
+        [HttpPost("match/{matchId}/remove-from-storage")]
+        public void RemoveFromStorage(long matchId)
+        {
+            _logger.LogInformation($"Received request for removal from storage of match [ {matchId} ]");
+
+            var instruction = new DemoRemovalInstruction
+            {
+                MatchId = matchId,
+            };
+
+            _matchWriter.PublishMessage(instruction);
+        }
+    }
+}
