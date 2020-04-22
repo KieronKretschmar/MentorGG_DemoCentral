9894f621384cc71903b09bf6d8995337653b9504
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-04-22 21:51:47 +0200

    Add test for new refactored method; Add possible mocks for rabbit connections

--- DemoCentralTest/DemoFileWorkerTests.cs
+++ DemoCentralTest/DemoFileWorkerTests.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataBase.DatabaseClasses;
+using DataBase.Enumerals;
 using DemoCentral;
+using DemoCentral.Communication.Rabbit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RabbitCommunicationLib.Interfaces;
+using RabbitCommunicationLib.TransferModels;
 
 namespace DemoCentralTests
 {
@@ -21,9 +29,39 @@ namespace DemoCentralTests
         }
 
         [TestMethod]
-        public void ReceivingZipFailedMessageUpdatesStatus()
+        public async System.Threading.Tasks.Task ReceivingZipFailedMessageUpdatesStatusAsync()
         {
+            var testMatchId = 123456789;
 
+            short timesCalled = 0; 
+
+            var mockRPCConnection = new MockRPCQueueConnection();
+            var mockQueueInterface = new Mock<IInQueueDBInterface>();
+            var mockDbInterface = new Mock<IDemoCentralDBInterface>();
+            var mockLogger = new Mock<ILogger<DemoFileWorker>>();
+            var mockProducer = new Mock<IProducer<RedisLocalizationInstruction>>();
+
+            mockDbInterface.Setup(x => x.SetFileWorkerStatus(It.IsAny<Demo>(), DemoFileWorkerStatus.UnzipFailed)).Callback(()=> timesCalled++);
+            mockDbInterface.Setup(x => x.SetFileWorkerStatus(testMatchId, DemoFileWorkerStatus.UnzipFailed)).Callback(()=> timesCalled++);
+
+            var services = new ServiceCollection();
+            services.AddTransient<IInQueueDBInterface>(s => mockQueueInterface.Object);
+            services.AddTransient<IDemoCentralDBInterface>(s => mockDbInterface.Object);
+            services.AddTransient<ILogger<DemoFileWorker>>(s => mockLogger.Object);
+            services.AddTransient<IProducer<RedisLocalizationInstruction>>(s => mockProducer.Object);
+
+
+            var analyzeReport = new DemoAnalyzeReport
+            {
+                MatchId = testMatchId
+            };
+
+
+
+            var test = new DemoFileWorker(mockRPCConnection, services.BuildServiceProvider());
+            await test.HandleMessageAsync(new RabbitMQ.Client.Events.BasicDeliverEventArgs(), analyzeReport);
+
+            Assert.IsTrue(timesCalled == 1);
         }
 
         [TestMethod]
@@ -53,7 +91,7 @@ namespace DemoCentralTests
 
 
 
-        private static readonly string successfulResponseJSON = 
+        private static readonly string successfulResponseJSON =
             "{\"Success\": true,  \"Version\": \"testVersion\",  \"BlobDownloadFailed\": false,  \"Unzipped\": true,  \"DuplicateChecked\": true,\"IsDuplicate\": false,  \"UploadedToRedis\": true,  \"FramesPerSecond\": 1,  \"Hash\": \"testHash\"}";
     }
 }
---
+++ DemoCentralTest/MockRabbitConnection.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using RabbitCommunicationLib.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+
+namespace DemoCentralTests
+{
+    public class MockRabbitConnection : IQueueConnection
+    {
+        public IConnection Connection { get; set; }
+        public string Queue { get; set; } = "mockedQueue";
+
+        public MockRabbitConnection()
+        {
+            var mockConnection = new Mock<IConnection>();
+            var mockChannel = new Mock<IModel>();
+
+
+            mockConnection.Setup(x => x.CreateModel()).Returns(mockChannel.Object);
+
+            Connection = mockConnection.Object;
+        }
+    }
+
+
+    public class MockRPCQueueConnection : IRPCQueueConnections
+    {
+        public IQueueConnection ConsumeConnection { get; set; } = new MockRabbitConnection();
+        public IQueueConnection ProduceConnection { get; set; } = new MockRabbitConnection();
+    }
+}
