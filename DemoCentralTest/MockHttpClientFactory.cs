14a63479dabb07ebec0375578c2e808c2884f92a
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-05-26 17:50:37 +0200

    Add option for mocking HttpClient and IHttpClientFactory

---
+++ DemoCentralTest/MockHttpClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace DemoCentralTests
+{
+    class MockHttpClientFactory : IHttpClientFactory
+    {
+        private readonly Dictionary<string,HttpClient> _clients;
+        public MockHttpClientFactory()
+        {
+            _clients = new Dictionary<string, HttpClient>();
+        }
+
+        public void RegisterClient(string name, HttpClient client)
+        {
+            _clients[name] = client;
+        }
+
+
+        public HttpClient CreateClient(string name)
+        {
+            return _clients[name];
+        }
+    }
+}
